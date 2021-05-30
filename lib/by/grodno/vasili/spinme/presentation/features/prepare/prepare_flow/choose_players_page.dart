ea25a4c3852608e4e4a300c2939a15103f7a9f17
Vasili <vasili.shkleinik@ah.nl>
2021-05-30 20:57:40 +0300

    Split widgets into separate files

--- lib/by/grodno/vasili/spinme/presentation/features/prepare/prepare_flow/choose_players_page.dart
+++ lib/by/grodno/vasili/spinme/presentation/features/prepare/prepare_flow/choose_players_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:spinme/by/grodno/vasili/spinme/presentation/preferences/game_preferences.dart';
-import 'package:spinme/by/grodno/vasili/spinme/presentation/utilities/utilities.dart';
+
+import 'names_form_widget.dart';
 
 const routeChoosePlayers = "choosePlayers";
 
@@ -14,123 +14,5 @@ class ChoosePlayersPage extends StatelessWidget {
   final Function(List<String>) onPlayersChosen;
 
   @override
-  Widget build(BuildContext context) {
-    return ChooseNamesForm(onPlayersChosen: onPlayersChosen);
-  }
-}
-
-class ChooseNamesForm extends StatefulWidget {
-  const ChooseNamesForm({Key? key, required this.onPlayersChosen}) : super(key: key);
-
-  final Function(List<String>) onPlayersChosen;
-
-  @override
-  ChooseNamesFormState createState() => ChooseNamesFormState();
-}
-
-class ChooseNamesFormState extends State<ChooseNamesForm> {
-  final _formKey = GlobalKey<FormState>();
-  final List<String> _names = [];
-  var _nameFields = <TextFormField>[];
-
-  @override
-  void initState() {
-    _nameFields.add(_createNameField());
-    _nameFields.add(_createNameField());
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              itemCount: _nameFields.length,
-              itemBuilder: (context, index) => _nameFields[index],
-            ),
-          ),
-          _createPlusButton(context),
-          _createSubmitButton(context)
-        ],
-      ),
-    );
-  }
-
-  Widget _createPlusButton(BuildContext context) {
-    final Function()? action = _nameFields.length >= GamePreferences.maxNumberOfPlayers
-        ? null
-        : () {
-            if (_nameFields.hasNonFilledField()) {
-              context.snack("You have non-filled fields");
-            } else {
-              _addNameField();
-            }
-          };
-    return ElevatedButton(
-      onPressed: action,
-      child: Text('+'),
-    );
-  }
-
-  Widget _createSubmitButton(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 16.0),
-      child: ElevatedButton(
-        onPressed: () {
-          if (_formKey.currentState!.validate()) {
-            _formKey.currentState!.save();
-            _submitForm();
-          } else {
-            context.snack("Your form is invalid!");
-          }
-        },
-        child: Text('Next'),
-      ),
-    );
-  }
-
-  void _submitForm() {
-    final areDuplicates = _names.length != _names.toSet().length;
-    if (areDuplicates) {
-      context.snack("All names should be different");
-      _names.clear();
-    } else {
-      widget.onPlayersChosen(_names);
-    }
-  }
-
-  void _addNameField() {
-    setState(() {
-      _nameFields.add(_createNameField());
-    });
-  }
-
-  TextFormField _createNameField() {
-    return TextFormField(
-      onTap: () {
-        setState(() {});
-      },
-      controller: TextEditingController(),
-      onSaved: (value) {
-        _names.add(value!);
-      },
-      decoration: const InputDecoration(
-        hintText: 'Enter player name',
-        labelText: 'Name',
-      ),
-      keyboardType: TextInputType.text,
-      validator: _validator,
-    );
-  }
-
-  String? _validator(value) =>
-      value.length < GamePreferences.minCharactersInPlayerName ? 'Please enter name longer than 3 symbols' : null;
-}
-
-extension TextFormFieldHasEmptyField on List<TextFormField> {
-  bool hasNonFilledField() =>
-      this.any((field) => field.controller!.value.text.length < GamePreferences.minCharactersInPlayerName);
+  Widget build(BuildContext context) => NamesFormWidget(onPlayersChosen: onPlayersChosen);
 }
--- lib/by/grodno/vasili/spinme/presentation/features/prepare/prepare_flow/choose_tasks_page.dart
+++ lib/by/grodno/vasili/spinme/presentation/features/prepare/prepare_flow/choose_tasks_page.dart
@@ -1,13 +1,8 @@
 import 'package:domain/domain_module.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:spinme/by/grodno/vasili/spinme/presentation/features/prepare/bloc/prepare_bloc.dart';
-import 'package:spinme/by/grodno/vasili/spinme/presentation/features/prepare/bloc/prepare_state.dart';
-import 'package:spinme/by/grodno/vasili/spinme/presentation/utilities/converters.dart';
-import 'package:spinme/by/grodno/vasili/spinme/presentation/utilities/utilities.dart';
 
-import 'checkable_task_item.dart';
+import 'tasks_widget.dart';
 
 const routeChooseTasks = "chooseTasks";
 
@@ -21,76 +16,4 @@ class ChooseTasksPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => TasksWidget(onTasksChosen: onTasksChosen);
-}
-
-class TasksWidget extends StatefulWidget {
-  const TasksWidget({Key? key, required this.onTasksChosen}) : super(key: key);
-
-  final Function(List<Task>) onTasksChosen;
-
-  @override
-  _TasksWidgetState createState() => _TasksWidgetState();
-}
-
-class _TasksWidgetState extends State<TasksWidget> {
-  final log = getLogger();
-  final Map<int, bool> _changedTasksIds = {};
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocBuilder<PrepareBloc, PrepareState>(
-      builder: (context, state) {
-        final tasks = state.tasks;
-        final isLoading = state.isLoading;
-
-        if (isLoading) return Center(child: CircularProgressIndicator());
-        return tasks == null ? Center(child: Text("Whoops! Strange state o_O")) : _buildTasksList(tasks);
-      },
-    );
-  }
-
-  Widget _buildTasksList(List<Task> tasks) => Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Expanded(
-            child: ListView.builder(
-                itemCount: tasks.length,
-                itemBuilder: (context, index) {
-                  var task = tasks[index];
-                  return CheckableTaskItem(
-                    task: task,
-                    isChecked: task.isChecked,
-                    onCheck: (isChecked) {
-                      _changedTasksIds[task.id] = isChecked;
-                    },
-                  );
-                }),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ElevatedButton(
-              onPressed: () {
-                _onNextClicked(tasks);
-              },
-              child: Text("Let's play!"),
-            ),
-          )
-        ],
-      );
-
-  void _onNextClicked(List<Task> tasks) {
-    final Set<Task> changedTasks = _changedTasksIds.entries.map((e) {
-      final task = tasks.firstWhere((element) => element.id == e.key);
-      return task.copyWithCheck(e.value);
-    }).toSet();
-    final Set<Task> updatedTasks = Set.from(changedTasks)..addAll(tasks);
-    final checkedTasksIds = updatedTasks.where((element) => element.isChecked).map((e) => e.id).toList();
-    log.i(message: "Chosen tasks ids: $checkedTasksIds");
-    final minimumNumberTasks = 3;
-    if (checkedTasksIds.length > minimumNumberTasks) {
-      widget.onTasksChosen(updatedTasks.toList());
-    } else {
-      context.snack("Please chose more that $minimumNumberTasks");
-    }
-  }
 }
