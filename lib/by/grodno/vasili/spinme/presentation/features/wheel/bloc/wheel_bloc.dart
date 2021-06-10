1a6b8dc2491d5ca75a638f09a9d3b548c9893677
Vasili <grodno.by@mail.ru>
2021-06-10 19:52:31 +0300

    Wheel bloc refactoring - separate methods for reducers

--- lib/by/grodno/vasili/spinme/presentation/features/wheel/bloc/wheel_bloc.dart
+++ lib/by/grodno/vasili/spinme/presentation/features/wheel/bloc/wheel_bloc.dart
@@ -12,22 +12,25 @@ class WheelBloc extends Bloc<WheelEvent, WheelState> {
 
   @override
   Stream<WheelState> mapEventToState(WheelEvent event) async* {
-    if (event is SpinFinished) {
-      final task = coordinator.pickTask();
-      final player = coordinator.pickPlayer(event.item.id);
-      yield state.copyWith(
-        label: "${(await player).name} do the task!",
-        pickedPlayer: await player,
-        pickedTask: await task,
-      );
-    } else if (event is SpinStarted) {
-      yield state.copyWith(label: "Yeeeha!");
-    } else if (event is ConfigureWheel) {
-      final players = await coordinator.getChosenPlayers();
-      final items = players.map((e) => WheelItem(id: e.id, label: e.name)).toList();
-      yield state.copyWith(items: items);
-    } else {
-      throw UnimplementedError("Unknown event: $event");
-    }
+    if (event is SpinFinished) yield* _mapSpinFinished(event);
+    else if (event is SpinStarted) yield state.copyWith(label: "Yeeeha!");
+    else if (event is ConfigureWheel) yield* _mapConfigureWheel(event);
+    else throw UnimplementedError("Unknown wheel-event: $event");
+  }
+
+  Stream<WheelState> _mapSpinFinished(SpinFinished event) async* {
+    final task = coordinator.pickTask();
+    final player = coordinator.pickPlayer(event.item.id);
+    yield state.copyWith(
+      label: "${(await player).name} do the task!",
+      pickedPlayer: await player,
+      pickedTask: await task,
+    );
+  }
+
+  Stream<WheelState> _mapConfigureWheel(ConfigureWheel event) async* {
+    final players = await coordinator.getChosenPlayers();
+    final items = players.map((e) => WheelItem(id: e.id, label: e.name)).toList();
+    yield state.copyWith(items: items);
   }
 }
