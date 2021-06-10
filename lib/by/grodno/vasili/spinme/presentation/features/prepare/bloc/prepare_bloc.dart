5081853fdd36ed3a638a25c9a3938b1a2f57f04b
Vasili <grodno.by@mail.ru>
2021-06-10 05:57:55 +0300

    Prepare bloc refactoring - separate methods for reducers

--- lib/by/grodno/vasili/spinme/presentation/features/prepare/bloc/prepare_bloc.dart
+++ lib/by/grodno/vasili/spinme/presentation/features/prepare/bloc/prepare_bloc.dart
@@ -12,44 +12,64 @@ class PrepareBloc extends Bloc<PrepareEvent, PrepareState> {
 
   @override
   Stream<PrepareState> mapEventToState(PrepareEvent event) async* {
-    if (event is LoadTasks) {
-      yield state.copyWith(isLoading: true);
-      final tasks = await coordinator.getAllTasks();
-      yield state.copyWith(isLoading: false, tasks: tasks);
-    } else if (event is NamesChosen) {
-      final List<Player> players = event.names.asMap().entries.map((entry) => Player(entry.key, entry.value)).toList();
-      await coordinator.replaceAllPlayers(players);
-      log.d(message: "${players.length} players replaced.");
-    } else if (event is TasksChosen) {
-      var tasks = event.tasks;
-      await coordinator.saveTasks(tasks);
-      log.d(message: "${tasks.length} tasks are saved.");
-    } else if (event is TaskEdited) {
-      final updatedTask = event.newTask;
-      await coordinator.saveTask(updatedTask);
-      final List<Task> newList = List.from(state.tasks!);
-      final index = newList.indexWhere((element) => element.id == updatedTask.id);
-      newList[index] = updatedTask;
-      yield state.copyWith(tasks: newList);
-    } else if (event is DeleteTask) {
-      final idToRemove = event.id;
-      await coordinator.deleteTask(idToRemove);
-      final List<Task> newList = List.from(state.tasks!);
-      newList.removeWhere((task) {
-        log.d(message: "Task $task deleted.");
-        return task.id == idToRemove;
-      });
-      yield state.copyWith(tasks: newList);
-    } else if (event is LoadPlayers) {
-      yield state.copyWith(isLoading: true);
-      final players = await coordinator.getAllPlayers();
-      yield state.copyWith(players: players, isLoading: false);
-    } else if (event is TaskAdded) {
-      final task = event.task;
-      await coordinator.saveTask(task);
-      log.d(message: "Task $task saved.");
-      final List<Task> newList = List.from(state.tasks!)..add(task);
-      yield state.copyWith(tasks: newList);
-    }
+    if (event is LoadTasks) yield* _mapLoadTasks();
+    else if (event is NamesChosen) await _createAndSavePlayers(event);
+    else if (event is TasksChosen) await _saveTasks(event);
+    else if (event is TaskEdited) yield* _mapTaskEdited(event);
+    else if (event is DeleteTask) yield* _mapDeleteTask(event);
+    else if (event is LoadPlayers) yield* _mapLoadPlayers();
+    else if (event is TaskAdded) yield* _mapTaskAdded(event);
+  }
+
+  Stream<PrepareState> _mapLoadTasks() async* {
+    yield state.copyWith(isLoading: true);
+    final tasks = await coordinator.getAllTasks();
+    yield state.copyWith(isLoading: false, tasks: tasks);
+  }
+
+  Future _createAndSavePlayers(NamesChosen event) async {
+    final List<Player> players = event.names.asMap().entries.map((entry) => Player(entry.key, entry.value)).toList();
+    await coordinator.replaceAllPlayers(players);
+    log.d(message: "${players.length} players replaced.");
+  }
+
+  Future _saveTasks(TasksChosen event) async {
+    final tasks = event.tasks;
+    await coordinator.saveTasks(tasks);
+    log.d(message: "${tasks.length} tasks are saved.");
+  }
+
+  Stream<PrepareState> _mapTaskEdited(TaskEdited event) async* {
+    final updatedTask = event.newTask;
+    await coordinator.saveTask(updatedTask);
+    final List<Task> newList = List.from(state.tasks!);
+    final index = newList.indexWhere((element) => element.id == updatedTask.id);
+    newList[index] = updatedTask;
+    yield state.copyWith(tasks: newList);
+  }
+
+  Stream<PrepareState> _mapDeleteTask(DeleteTask event) async* {
+    final idToRemove = event.id;
+    await coordinator.deleteTask(idToRemove);
+    final List<Task> newList = List.from(state.tasks!);
+    newList.removeWhere((task) {
+      log.d(message: "Task $task deleted.");
+      return task.id == idToRemove;
+    });
+    yield state.copyWith(tasks: newList);
+  }
+
+  Stream<PrepareState> _mapLoadPlayers() async* {
+    yield state.copyWith(isLoading: true);
+    final players = await coordinator.getAllPlayers();
+    yield state.copyWith(players: players, isLoading: false);
+  }
+
+  Stream<PrepareState> _mapTaskAdded(TaskAdded event) async* {
+    final task = event.task;
+    await coordinator.saveTask(task);
+    log.d(message: "Task $task saved.");
+    final List<Task> newList = List.from(state.tasks!)..add(task);
+    yield state.copyWith(tasks: newList);
   }
 }
